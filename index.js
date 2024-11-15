55d14add2b34a6eafb3a5b5d156e4dd407a38c6d
Aditya Raj Bisoyi <159037169+adityarajbisoyi@users.noreply.github.com>
2024-11-15 06:57:32 +0530

    last commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const api = "https://ssd-api.jpl.nasa.gov/cad.api";
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
